2a399db3098b861c37de2045ab52dfc676b1701f
kaka7 <1457844118@qq.com>
2018-03-18 23:43:02 +0800

    git learning

---
+++ note/20180318_git.sh
@@ -0,0 +1,26 @@
+#new local rep
+mkdir local_rep_name && cd
+git init
+git add
+git commit -m ""
+git remote add origin URL
+git push -u origin master
+
+#add local change file
+git add 
+git pull URL
+git pull --rebase origin master
+git push -u origin master
+
+#maybe help
+git remote set-url origin https://github.com/kaka7/marathon.git
+git remote rm origin
+git remote add origin https://github.com/kaka7/marathon.git
+git remote -v
+
+git pull = git fetch + git merge
+git pull --rebase = git fetch + git rebase
+git status
+
+
+
